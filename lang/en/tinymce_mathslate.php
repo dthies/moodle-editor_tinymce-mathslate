1a6bc8d33bd31ff430bca74af2b241e200062d2f
Daniel Thies <dthies@ccal.edu>
2014-03-02 22:46:12 -0600

    Update tinymce_mathslate.php
    Add missing nomathjax string

--- lang/en/tinymce_mathslate.php
+++ lang/en/tinymce_mathslate.php
@@ -39,6 +39,7 @@ $string['requiretex'] = 'Require TeX filter';
 $string['requiretex_desc'] = 'If enabled the Mathslate button is visible only when the TeX filter is enabled in the editor context. Disable if you want it to appear globally (normal if MathJax is included in header sitewide).';
 $string['mathjaxurl'] = 'MathJax url';
 $string['mathjaxurl_desc'] = 'The url for the MathJax.js file that mathslate should load if MathJax is not already present and configured (probably true if you are using the Moodle TeX filter). The default downloads from the internet using http. If your site uses https use "https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js" or better install a local copy.';
+$string['nomathjax'] = '<p>MathJax does not seem to be present on this site. In order to use this plugin MathJax needs to be configured. MathJax is an opensource software library that is capable of displaying mathmatics in any javascript enabled browser.  Mathslate uses it to render mathematics within the editor. Therefore you should check with the system administrator of this site to see whether MathJax may be installed. See <a href="http://mathjax.org" target="_blank">mathjax.org</a> for more instructions.</p>';
 
-/* All lang strings used from TinyMCE JavaScript code must be named 'pluginname:stringname', no need to create langs/en_dlg.js */
+/* All lang strings used from TinyMCE JavaScript code must be named 'pluginname:stringname', nono need to create langs/en_dlg.js */
 $string['mathslate:desc'] = 'Insert equation';
