6b93caa551ba14e6136be485b3412a98991bb5ed
Daniel Thies <dthies@ccal.edu>
2014-03-06 17:53:36 -0600

    update

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // The current plugin version (Date: YYYYMMDDXX).
-$plugin->version   = 2014022701;
+$plugin->version   = 2014022702;
 // Required Moodle version.
 $plugin->requires  = 2013040500;
 // Full name of the plugin (used for diagnostics).
