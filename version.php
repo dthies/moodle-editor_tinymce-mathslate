abef256287bde2d9e09562fb2ea242d10d44d159
Daniel Thies <dthies@ccal.edu>
2014-03-06 17:41:31 -0600

    Update version.php

--- version.php
+++ version.php
@@ -30,5 +30,5 @@ $plugin->version   = 2014022601;
 $plugin->requires  = 2013111801;
 // Full name of the plugin (used for diagnostics).
 $plugin->component = 'tinymce_mathslate';
-$plugin->maturity = 'Beta';
+$plugin->maturity = MATURITY_BETA;
 $plugin->release = '1.0Beta1';
