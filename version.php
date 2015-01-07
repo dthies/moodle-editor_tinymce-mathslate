2a8ce00d2ab7d88db2105b555c0dffc12bbabe21
Daniel Thies <dthies@ccal.edu>
2015-01-07 09:30:50 -0600

    Update version.php

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 // The current plugin version (Date: YYYYMMDDXX).
 $plugin->version   = 2015010600;
 // Required Moodle version.
-$plugin->requires  = 2012050100;
+$plugin->requires  = 2013101800;
 // Full name of the plugin (used for diagnostics).
 $plugin->component = 'tinymce_mathslate';
 $plugin->maturity = MATURITY_BETA;
