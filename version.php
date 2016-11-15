0ebe9265a013a23431b793b9182f3ed4722baca9
Daniel Thies <dthies@ccal.edu>
2016-11-15 12:26:28 -0600

    Update CSS for Boost theme

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // The current plugin version (Date: YYYYMMDDXX).
-$plugin->version   = 2015041700;
+$plugin->version   = 2015041701;
 // Required Moodle version.
 $plugin->requires  = 2013101800;
 // Full name of the plugin (used for diagnostics).
